d184b160d7e4442ab3e6986fa9b2ec2335c47e45
viktorqanakiev <viktor.qanakiev@gmail.com>
2024-04-25 16:43:32 +0300

    updateing

--- dotSideNavScript.js
+++ dotSideNavScript.js
@@ -10,30 +10,36 @@ $(document).ready(function(){
     function dotnavigation(){
              
         var numSections = $('section').length;
-        
+        // console.log("Number of sections:" + " " + numSections);
         $('#dot-nav li a').removeClass('active').parent('li').removeClass('active');     
         $('section').each(function(i,item){
           var ele = $(item), nextTop;
-        
+          // console.log("ele offset:" + " " + ele.offset().top)
           // console.log(ele.next().html());
-          
+         
           if (typeof ele.next().offset() != "undefined") {
             nextTop = ele.next().offset().top;
+            // console.log("Not undefined nextTop:" + " " + nextTop);
           }
           else {
             nextTop = $(document).height();
+            // console.log("Undefined nextTop:" + " " + nextTop);
           }
+          //console.log(nextTop);
           
           if (ele.offset() !== null) {
             thisTop = ele.offset().top - ((nextTop - ele.offset().top) / numSections);
+            // console.log("This top:" + " " + thisTop);
           }
           else {
             thisTop = 0;
+            // console.log("This top zero:" + " " + thisTop);
           }
           
           var docTop = $(document).scrollTop();
-          
-          if(docTop >= thisTop && (docTop < nextTop)){
+          // console.log("DocTop:" + " " + docTop);
+          // console.log("----------")
+          if(docTop >= ele.offset().top && (docTop < nextTop)){
             $('#dot-nav li').eq(i).addClass('active');
           }
         });   
@@ -50,7 +56,7 @@ $(document).ready(function(){
         ele = $(id);
         posi = ($(ele).offset()||0).top - padding;
       
-        $('html, body').animate({scrollTop:posi}, 'slow');
+        $('html, body').animate({scrollTop:posi}, 0);
       
         return false;
     });
